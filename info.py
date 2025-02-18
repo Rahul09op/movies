df633b9fd627f3d886878f2cde5e25aaac540492
Rahul09op <rahulprajapaticr7@gmail.com>
2025-02-19 00:04:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7427683212:AAGlWLl8SjRLhpl6FuDROGL4sQEpdlh
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1002412815850').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/Itz_Rahulrp9") # ADMIN USERNAME
+USERNAME = environ.get('USERNAME', "https://t.me/Innocent_babe_dead") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002466799122'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/RCBMoviesBot')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002447231712').split()]
